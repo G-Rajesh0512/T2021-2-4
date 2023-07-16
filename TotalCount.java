a6a025bae888f42b98a6264109ec8a24bade72d3
G-Rajesh0512 <139709257+G-Rajesh0512@users.noreply.github.com>
2023-07-17 04:19:22 +0530

    Add files via upload

---
+++ TotalCount.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class TotalCount {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        int[] multiples = new int[10]; 
+     
+        for (int number : numbers) {
+            for (int i = 1; i <= 9; i++) {
+                if (number % i == 0) {
+                    multiples[i]++;
+                }
+            }
+        }
+        
+       
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(i + ": " + multiples[i]);
+        }
+    }
+}
